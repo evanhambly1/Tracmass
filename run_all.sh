87d50cb81a6bb8ecaff8eb6f5be6bfd9fcdcc7a0
Evan Hambly <emdh1n25@login01.anemone.noc.ac.uk>
2026-04-17 19:55:27 +0100

    update

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for YEAR in {1994..1999}; do
+for YEAR in {1990..1999}; do
     for MONTH in $(seq -w 1 12); do
-        JOBNAME="${MONTH}_${YEAR}_1m_eORCA1"
+        JOBNAME="${MONTH}_${YEAR}_1m_eORCA025"
         echo "Submitting $JOBNAME"
-        sbatch --job-name=$JOBNAME --export=YEAR=$YEAR,MONTH=$MONTH submit_tracmass_eORCA1_all.slurm
+        sbatch --job-name=$JOBNAME --export=YEAR=$YEAR,MONTH=$MONTH submit_tracmass_eORCA025_all.slurm
     done
 done
